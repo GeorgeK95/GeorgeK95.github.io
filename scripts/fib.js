2f09f541fce7a21e8b5711e68f2737287b6386f1
georgi.kopchev <georgi.kopchev@synergygfs.com>
2020-02-25 13:08:07 +0200

    fib test3

--- scripts/fib.js
+++ scripts/fib.js
@@ -1,10 +1,25 @@
 TIMEOUT = 4000;
 
 window.addEventListener("load", function () {
-    setTimeout(onReadMoreButtonClicked, TIMEOUT);
+    setTimeout(simulateClick, TIMEOUT);
 });
 
 function onReadMoreButtonClicked() {
     window.location = "https://my.fibank.bg/redirect/psd2/";
     // document.getElementById('btnReadMoreId').click()
+}
+
+function simulateClick() {
+    const event = new MouseEvent('click', {
+        view: window,
+        bubbles: true,
+        cancelable: true
+    });
+    const cb = document.getElementById('btnReadMoreId');  //whichever element you want to click
+    const cancelled = !cb.dispatchEvent(event);
+    if (cancelled) {
+        console.log("A handler called preventDefault.")
+    } else {
+        console.log("None of the handlers called preventDefault.")
+    }
 }
